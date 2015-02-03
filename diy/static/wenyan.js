48b1b792728a61532b12dbc3ea269e5d98e126ea
Dingyuan Wang <abcdoyle888@gmail.com>
2015-02-03 20:29:54 +0800

    improve css; add progress bar

---
+++ diy/static/wenyan.js
@@ -0,0 +1,22 @@
+var languncertain = false;
+function AutoGrow(oField) {
+if (oField.scrollHeight > oField.clientHeight) {
+oField.style.height = oField.scrollHeight + oField.clientTop*2 + 1 + "px";
+}}
+function PBRun() {
+var PBar = document.getElementById('progressbar');
+var origwid = document.getElementById('toutput').offsetWidth;
+PBar.style["transition-duration"] = 90 * document.getElementById("tinput").value.length + "ms";
+PBar.style["width"] = origwid + "px";
+}
+document.addEventListener('DOMContentLoaded', function() {
+if (languncertain) {
+document.getElementById("changedir").onclick = function() {
+document.getElementById("hidden-lang").value = languncertain;
+document.getElementById("translateform").submit();
+return false;
+}
+}
+AutoGrow(document.getElementById('tinput'));
+document.getElementById("ctrlsubmit").onclick = PBRun;
+});
