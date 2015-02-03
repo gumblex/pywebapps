014aa0a00bd94b7b49c0763d4473738a81abeef6
Dingyuan Wang <abcdoyle888@gmail.com>
2015-02-03 21:01:17 +0800

    use linear regression

--- diy/static/wenyan.js
+++ diy/static/wenyan.js
@@ -6,7 +6,7 @@ oField.style.height = oField.scrollHeight + oField.clientTop*2 + 1 + "px";
 function PBRun() {
 var PBar = document.getElementById('progressbar');
 var origwid = document.getElementById('toutput').offsetWidth;
-PBar.style["transition-duration"] = 90 * document.getElementById("tinput").value.length + "ms";
+PBar.style["transition-duration"] = 24 * document.getElementById("tinput").value.length + 3347 + "ms";
 PBar.style["width"] = origwid + "px";
 }
 document.addEventListener('DOMContentLoaded', function() {
