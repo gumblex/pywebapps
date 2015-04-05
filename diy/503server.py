836de7598d3f16963af69625ccf93a8fc91a886f
Dingyuan Wang <abcdoyle888@gmail.com>
2015-04-05 17:47:50 +0800

    apache log format for 503

--- diy/503server.py
+++ diy/503server.py
@@ -18,8 +18,8 @@ class Unbuffered(object):
 	def __getattr__(self, attr):
 		return getattr(self.stream, attr)
 
-sys.stdout = Unbuffered(sys.stdout)
-sys.stderr = Unbuffered(sys.stderr)
+#sys.stdout = Unbuffered(sys.stdout)
+#sys.stderr = Unbuffered(sys.stderr)
 
 HTMLFILE = open(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'diy/templates/e503.html'), 'rb').read()
 HTMLLEN = str(len(HTMLFILE))
@@ -29,14 +29,25 @@ class ThreadingHTTPServer(ThreadingMixIn, http.server.HTTPServer):
 
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
 
+	def send_response(self, code, message=None):
+		self.send_response_only(code, message)
+		self.send_header('Server', self.version_string())
+		self.send_header('Date', self.date_time_string())
+
+	def log_message(self, format, *args):
+		sys.stderr.write('%s - - [%s] %s "%s" "%s"\n' % (self.headers.get('X-Forwarded-For', self.address_string()), self.log_date_time_string(), format%args, self.headers.get('Referer', '-'), self.headers.get('User-Agent', '-')))
+		sys.stderr.flush()
+
 	def do_HEAD(self):
 		self.send_response(503)
+		self.log_request(503)
 		self.send_header('Retry-After', '300')
 		self.end_headers()
 		return
 
 	def do_GET(self):
 		self.send_response(503)
+		self.log_request(503, HTMLLEN)
 		self.send_header('Retry-After', '300')
 		self.send_header('Content-Length', HTMLLEN)
 		self.send_header('Content-Type', 'text/html')
@@ -45,10 +56,6 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
 		return
 
 	do_POST = do_GET
-
-	def log_message(self, format, *args):
-		sys.stderr.write("%s - - [%s] %s\n" % (self.headers.get('X-Forwarded-For') or self.address_string(), self.log_date_time_string(), format%args))
-
 
 def run(server_class=ThreadingHTTPServer,
 		handler_class=HTTPHandler):
