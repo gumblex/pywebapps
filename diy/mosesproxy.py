157eba2b06c7958e85da60d893a57a12df92439f
Dingyuan Wang <abcdoyle888@gmail.com>
2016-01-31 15:38:16 +0800

    mosesproxy: add command line option

--- diy/mosesproxy.py
+++ diy/mosesproxy.py
@@ -139,6 +139,10 @@ if __name__ == '__main__':
             if not ping():
                 sys.exit(1)
             sys.stdout.write(translate(sys.stdin.read(), 'm2c.raw') + '\n')
+        elif sys.argv[1] == 'modelname':
+            if not ping():
+                sys.exit(1)
+            sys.stdout.write((modelname() or '') + '\n')
     else:
         if not ping():
             sys.exit(1)
